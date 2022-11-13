4e1e52a359c30ea41f686c81f999679d1be47b95
xzjbit715 <xzjbit715@gmail.com>
2022-11-13 20:06:30 +0800

    Create MyUserscript.user.js

---
+++ MyUserscript.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         My Example Userscript
+// @namespace    https://github.com/xzjbit715
+// @version      0.3
+// @description  try to take over the world!
+// @author       xzjbit715
+// @match        http*://example.com/*
+// @icon         none
+// @grant        GM_addElement
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('MyUserscript 0.3');
+
+    GM_addElement(document.querySelector('div'), 'input', {
+        type: 'button',
+        value: 'this button is added by userscript'
+    });
+})();
