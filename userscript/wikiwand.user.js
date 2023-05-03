9690d03ffd5bbb85de72d4df49f4be7cba0d848f
xzjbit715 <xzjbit715@gmail.com>
2023-05-04 00:12:50 +0800

    Update wikiwand.user.js

--- userscript/wikiwand.user.js
+++ userscript/wikiwand.user.js
@@ -3,10 +3,10 @@
 // @namespace    https://github.com/xzjbit715/
 // @version      0.1
 // @description  修正當頁面自動轉換繁簡中文字時，超連結文字格式未正確套用
-// @author       You
+// @author       xzjbit715
 // @match        https://www.wikiwand.com/*
-// @downloadURL  https://github.com/xzjbit715/hello-world/blob/master/userscript/wikiwand.user.js
-// @updateURL    https://github.com/xzjbit715/hello-world/blob/master/userscript/wikiwand.user.js
+// @downloadURL  https://github.com/xzjbit715/hello-world/raw/master/userscript/wikiwand.user.js
+// @updateURL    https://github.com/xzjbit715/hello-world/raw/master/userscript/wikiwand.user.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wikiwand.com
 // @grant        GM_addStyle
 // ==/UserScript==
